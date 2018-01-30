21c6b6bf4610b0a3417b1fe3614c51bd1e31a215
Aaron Haurwitz <aaron.haurwitz@gmail.com>
2018-01-29 20:16:40 -0500

    style more global things

--- components/Page.js
+++ components/Page.js
@@ -6,6 +6,13 @@ const Page = ({ children }) => (
     <style jsx global>{`
       html {
         background: lightgrey;
+        /*System fonts:*/
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+          Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',
+          'Segoe UI Symbol';
+      }
+      h1 {
+        text-align: center;
       }
     `}</style>
   </div>

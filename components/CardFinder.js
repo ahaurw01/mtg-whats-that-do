d57244d7e506005121c26b14ab8387266cb3a545
Aaron Haurwitz <aaron.haurwitz@gmail.com>
2018-01-29 21:33:29 -0500

    create CardFinder component

---
+++ components/CardFinder.js
@@ -0,0 +1,117 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import Autosuggest from 'react-autosuggest';
+import Tile from './Tile';
+
+const getSuggestionValue = suggestion => suggestion;
+const renderSuggestion = suggestion => suggestion;
+
+export default class CardFinder extends Component {
+  static propTypes = {
+    onCardSelected: PropTypes.func.isRequired,
+  };
+
+  onChange = (event, { newValue }) => {
+    this.setState({
+      value: newValue,
+    });
+  };
+
+  onSuggestionSelected = (e, { suggestion }) => {
+    this.props.onCardSelected(suggestion);
+  };
+
+  onSuggestionsFetchRequested = ({ value }) => {
+    if (value.length < 3) {
+      this.setState({
+        suggestions: [],
+      });
+      return;
+    }
+
+    fetch(`https://api.scryfall.com/cards/autocomplete?q=${value}`)
+      .then(result => result.json())
+      .then(result =>
+        this.setState({
+          suggestions: result.data || [],
+        })
+      );
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      suggestions: [],
+    };
+  }
+
+  render() {
+    const { value, suggestions } = this.state;
+    const inputProps = {
+      autoFocus: true,
+      placeholder: 'Find a card...',
+      value,
+      onChange: this.onChange,
+    };
+    return (
+      <Tile>
+        <Autosuggest
+          suggestions={suggestions}
+          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+          onSuggestionSelected={this.onSuggestionSelected}
+          getSuggestionValue={getSuggestionValue}
+          renderSuggestion={renderSuggestion}
+          inputProps={inputProps}
+          alwaysRenderSuggestions
+        />
+        <style jsx global>{`
+          .react-autosuggest__container {
+            position: relative;
+          }
+          .react-autosuggest__input {
+            width: 300px;
+            padding: 12px;
+            border: 1px solid #333;
+            border-radius: 4px;
+            background: lightgrey;
+            font-size: 16px;
+          }
+          .react-autosuggest__input--open {
+            border-bottom-left-radius: 0;
+            border-bottom-right-radius: 0;
+          }
+          .react-autosuggest__input--focused {
+            outline: none;
+          }
+          .react-autosuggest__suggestions-container--open {
+            position: absolute;
+            top: 43px;
+            width: 300px;
+            border: 1px solid #333;
+            border-bottom-left-radius: 4px;
+            border-bottom-right-radius: 4px;
+          }
+          .react-autosuggest__suggestions-list {
+            margin: 0;
+            padding: 0;
+            list-style-type: none;
+          }
+          .react-autosuggest__suggestion {
+            padding: 12px;
+            cursor: pointer;
+            overflow: hidden;
+            background: lightgrey;
+          }
+          .react-autosuggest__suggestion:last-of-type {
+            border-bottom-left-radius: 4px;
+            border-bottom-right-radius: 4px;
+          }
+          .react-autosuggest__suggestion--highlighted {
+            background: darkgrey;
+          }
+        `}</style>
+      </Tile>
+    );
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Page from '../components/Page';
+import CardFinder from '../components/CardFinder';
 
 export default () => (
   <Page>
     <h1>What's that do?</h1>
+    <CardFinder onCardSelected={name => console.log(name)} />
   </Page>
 );

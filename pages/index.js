1872bbd0eebf748165eee6ecc6c0c8b95a204b33
Aaron Haurwitz <aaron.haurwitz@gmail.com>
2018-01-30 18:37:05 -0500

    render the CardManager on index page

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Page from '../components/Page';
-import CardFinder from '../components/CardFinder';
+import CardManager from '../components/CardManager';
 
 export default () => (
   <Page>
     <h1>What's that do?</h1>
-    <CardFinder onCardSelected={name => console.log(name)} />
+    <CardManager />
   </Page>
 );

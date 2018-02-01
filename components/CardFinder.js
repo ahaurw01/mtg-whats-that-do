7e37296bf1f6e69d0a7c49721cf8e71fecab5065
Aaron Haurwitz <aaron.haurwitz@gmail.com>
2018-01-31 21:52:37 -0500

    use Search in CardFinder

--- components/CardFinder.js
+++ components/CardFinder.js
@@ -1,8 +1,9 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
-import Autosuggest from 'react-autosuggest';
+import { Search, Input } from 'semantic-ui-react';
 import Tile from './Tile';
 
+const renderInputComponent = inputProps => <Input {...inputProps} fluid />;
 const getSuggestionValue = suggestion => suggestion;
 const renderSuggestion = suggestion => suggestion;
 
@@ -11,112 +12,65 @@ export default class CardFinder extends Component {
     onCardSelected: PropTypes.func.isRequired,
   };
 
-  onChange = (event, { newValue }) => {
-    this.setState({
-      value: newValue,
-    });
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      results: [],
+      loading: false,
+      value: '',
+    };
+  }
 
-  onSuggestionSelected = (e, { suggestion }) => {
-    this.setState({ value: '', suggestions: [] });
-    this.props.onCardSelected(suggestion);
-  };
-
-  shouldRenderSuggestions = value => {
-    return value.trim().length > 2;
-  };
-
-  onSuggestionsFetchRequested = ({ value, reason }) => {
-    if (value.length < 3 || reason === 'suggestion-selected') {
-      this.setState({
-        suggestions: [],
-      });
+  onSearchChange = (e, { value }) => {
+    this.setState({ value });
+    if (value.length < 3) {
+      this.setState({ results: [] });
       return;
     }
 
+    this.setState({ loading: true });
     fetch(`https://api.scryfall.com/cards/autocomplete?q=${value}`)
       .then(result => result.json())
       .then(result =>
         this.setState({
-          suggestions: result.data || [],
+          results: (result.data || []).map(name => ({ title: name })),
+          loading: false,
         })
       );
   };
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: '',
-      suggestions: [],
-    };
-  }
+  onResultSelect = (e, { result: { title: cardName } }) => {
+    this.props.onCardSelected(cardName);
+    this.setState({ results: [], value: '' });
+  };
 
   render() {
-    const { value, suggestions } = this.state;
-    const inputProps = {
-      autoFocus: true,
-      placeholder: 'Find a card...',
-      value,
-      onChange: this.onChange,
-    };
+    const { results, loading, value } = this.state;
     return (
       <Tile>
-        <Autosuggest
-          suggestions={suggestions}
-          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-          shouldRenderSuggestions={this.shouldRenderSuggestions}
-          onSuggestionSelected={this.onSuggestionSelected}
-          getSuggestionValue={getSuggestionValue}
-          renderSuggestion={renderSuggestion}
-          inputProps={inputProps}
-          alwaysRenderSuggestions
+        <Search
+          results={results}
+          fluid
+          input={
+            <Input
+              fluid
+              input={{
+                className: '',
+                tabIndex: '0',
+                autoComplete: 'off',
+                placeholder: 'Find a card...',
+                autoFocus: true,
+              }}
+            />
+          }
+          loading={loading}
+          minCharacters={3}
+          noResultsMessage="No cards found."
+          onSearchChange={this.onSearchChange}
+          onResultSelect={this.onResultSelect}
+          value={value}
         />
-        <style jsx global>{`
-          .react-autosuggest__container {
-            position: relative;
-          }
-          .react-autosuggest__input {
-            width: 100%;
-            padding: 12px;
-            border: 1px solid #333;
-            border-radius: 4px;
-            background: lightgrey;
-            font-size: 16px;
-          }
-          .react-autosuggest__input--open {
-            border-bottom-left-radius: 0;
-            border-bottom-right-radius: 0;
-          }
-          .react-autosuggest__input--focused {
-            outline: none;
-          }
-          .react-autosuggest__suggestions-container--open {
-            position: absolute;
-            top: 43px;
-            width: 100%;
-            border: 1px solid #333;
-            border-bottom-left-radius: 4px;
-            border-bottom-right-radius: 4px;
-          }
-          .react-autosuggest__suggestions-list {
-            margin: 0;
-            padding: 0;
-            list-style-type: none;
-          }
-          .react-autosuggest__suggestion {
-            padding: 12px;
-            cursor: pointer;
-            overflow: hidden;
-            background: lightgrey;
-          }
-          .react-autosuggest__suggestion:last-of-type {
-            border-bottom-left-radius: 4px;
-            border-bottom-right-radius: 4px;
-          }
-          .react-autosuggest__suggestion--highlighted {
-            background: darkgrey;
-          }
-        `}</style>
+        <style jsx global>{``}</style>
       </Tile>
     );
   }

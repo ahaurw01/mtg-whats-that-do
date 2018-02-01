d3ab1c20e533e3eb1f0328ee5a9aa55e96a6cb0b
Aaron Haurwitz <aaron.haurwitz@gmail.com>
2018-01-31 21:52:59 -0500

    fix Tile styling

--- components/Tile.js
+++ components/Tile.js
@@ -2,22 +2,30 @@ import PropTypes from 'prop-types';
 import { Segment } from 'semantic-ui-react';
 
 const Tile = ({ children }) => (
-  <div className="tile">
-    <Segment raised>{children}</Segment>
-    <style jsx>{`
-      .tile {
+  <div className="wtd-tile">
+    <Segment raised className="wtd-segment">
+      {children}
+    </Segment>
+    <style global jsx>{`
+      .wtd-tile {
         display: block;
+        margin: auto;
+      }
+
+      .wtd-segment {
         width: 300px;
         min-height: 480px;
-        margin: auto;
-        position: relative;
       }
+
       @media (min-width: 768px) {
-        .tile {
-          width: 350px;
-          min-height: 549px;
+        .wtd-tile {
           margin: 10px;
           float: left;
+        }
+
+        .wtd-segment {
+          width: 350px;
+          min-height: 549px;
         }
       }
     `}</style>

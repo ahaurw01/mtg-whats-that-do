18fe2c921bef55dbbeb7de7a053e33351a20e0fd
Aaron Haurwitz <aaron.haurwitz@gmail.com>
2018-01-31 22:00:44 -0500

    render Header instead of h1

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Page from '../components/Page';
 import CardManager from '../components/CardManager';
+import { Header } from 'semantic-ui-react';
 
 export default () => (
   <Page>
-    <h1>What's that do?</h1>
+    <Header as="h1" block>
+      What's that do?
+    </Header>
     <CardManager />
   </Page>
 );

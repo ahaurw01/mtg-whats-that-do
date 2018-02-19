f51aa0a41480bfce8965bbfbf68bce01ce8cc41d
Aaron Haurwitz <aaron.haurwitz@gmail.com>
2018-02-19 14:56:41 -0500

    deploy from npm script

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-[ -z "$FIREBASE_TOKEN" ] && echo "Must set FIREBASE_TOKEN" && exit 1;
-
-node_modules/.bin/next build;
-node_modules/.bin/next export;
-node_modules/.bin/firebase deploy --token $FIREBASE_TOKEN;

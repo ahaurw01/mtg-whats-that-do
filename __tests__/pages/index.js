ad3b5f71eb5ef27374400800e04f5d8eac41a349
Aaron Haurwitz <aaron.haurwitz@gmail.com>
2018-02-21 21:24:45 -0500

    link to issues, option to clear cards

---
+++ __tests__/pages/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Index from '../../pages/index';
+import CardManager from '../../components/CardManager';
+import MainHeader from '../../components/MainHeader';
+import { mount } from 'enzyme';
+
+describe('Index', () => {
+  test('cards are cleared', () => {
+    const wrapper = mount(<Index />);
+    const clearCards = jest.fn();
+    wrapper.find(CardManager).instance().clearCards = clearCards;
+
+    wrapper
+      .find(MainHeader)
+      .find('a')
+      .at(0)
+      .simulate('click');
+
+    expect(clearCards.mock.calls).toHaveLength(1);
+  });
+});

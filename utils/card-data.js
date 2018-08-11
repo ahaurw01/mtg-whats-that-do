bf83652a9c80fc586fde7bb88444c5c3d9483196
Aaron Haurwitz <aaron.haurwitz@gmail.com>
2018-08-11 15:07:12 -0400

    add getOracleData to card utils

--- utils/card-data.js
+++ utils/card-data.js
@@ -9,3 +9,29 @@ export const getImageUrl = (card, faceIndex = 0) => {
 };
 
 export const isDoubleFaced = card => !!card && !!card.card_faces;
+
+/**
+ * Extract oracle data from the card. There may be two items if the card is
+ * double faced.
+ *
+ * @param {Object} card   Card object.
+ *
+ * @return {Object[]|null} Array of oracle info.
+ */
+export const getOracleData = card => {
+  if (!card) return null;
+
+  const mapper = face => ({
+    color_identity: face.color_identity,
+    mana_cost: face.mana_cost,
+    name: face.name,
+    oracle_text: face.oracle_text,
+    power: face.power,
+    toughness: face.toughness,
+    type_line: face.type_line,
+    flavor_text: face.flavor_text,
+    loyalty: face.loyalty,
+  });
+
+  return isDoubleFaced(card) ? card.card_faces.map(mapper) : [mapper(card)];
+};

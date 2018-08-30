956b13c8b185b5341ceb6ffdeb5e064ba43b5b78
Aaron Haurwitz <aaron.haurwitz@gmail.com>
2018-08-30 08:53:51 -0400

    upgrade semantic ui react

--- components/Page.js
+++ components/Page.js
@@ -8,7 +8,7 @@ const Page = ({ children }) => (
       <meta charSet="utf-8" />
       <link
         rel="stylesheet"
-        href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"
+        href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.min.css"
       />
       <link
         href="//cdn.jsdelivr.net/npm/mana-font@1.4.1/css/mana.min.css"

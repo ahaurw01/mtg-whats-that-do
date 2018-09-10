248545077ade47a95b8ac17e59be011a4da13b73
Aaron Haurwitz <aaron.haurwitz@gmail.com>
2018-09-09 22:05:26 -0400

    use navigator.share if available

--- __tests__/components/ShareModal.test.js
+++ __tests__/components/ShareModal.test.js
@@ -65,4 +65,39 @@ describe('ShareModal', () => {
       );
     });
   });
+
+  describe('opening the modal', () => {
+    test('opens the modal if no share API is present', () => {
+      window.navigator.share = undefined;
+
+      const wrapper = mount(<ShareModal isOpen={false} onClose={jest.fn()} />);
+
+      expect(wrapper.find(Modal)).toHaveLength(1);
+      expect(wrapper.find(Modal).prop('open')).toBe(false);
+
+      wrapper.setProps({ isOpen: true });
+      wrapper.update();
+      expect(wrapper.find(Modal).prop('open')).toBe(true);
+    });
+  });
+
+  test('invokes navigator.share() instead if present', () => {
+    window.navigator.share = jest.fn();
+    const onClose = jest.fn();
+
+    const wrapper = mount(<ShareModal isOpen={false} onClose={onClose} />);
+
+    expect(wrapper.find(Modal)).toHaveLength(1);
+    expect(wrapper.find(Modal).prop('open')).toBe(false);
+
+    wrapper.setProps({ isOpen: true });
+    wrapper.update();
+    expect(wrapper.find(Modal).prop('open')).toBe(false);
+    expect(window.navigator.share).toHaveBeenCalledTimes(1);
+    expect(window.navigator.share).toHaveBeenCalledWith({
+      url: 'https://whatsthatdo.net',
+      title: "What's that do?",
+    });
+    expect(onClose).toHaveBeenCalledTimes(1);
+  });
 });

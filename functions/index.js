e20ccf52d3f0d0efa8cecdad3cf57c9f167cbd03
Aaron Haurwitz <aaron.haurwitz@gmail.com>
2018-09-19 09:01:20 -0400

    interact with the firestore

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,12 @@
 const crypto = require('crypto');
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 const express = require('express');
 const shortId = require('shortid');
+
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+db.settings({ timestampsInSnapshots: true });
 
 const app = express();
 
@@ -18,9 +23,27 @@ function makeShareCode(req, res) {
   const code = shortId.generate();
   const { cardNames } = req.body;
 
-  res.send({ code, cardNames });
+  const docRef = db
+    .collection('shares')
+    .doc(code)
+    .set({ cardNames })
+    .then(() => res.send({ code }))
+    .catch(err => res.status(500).send(err));
 }
 
 function getShare(req, res) {
-  res.send(req.params.code);
+  const { code } = req.params;
+
+  db
+    .collection('shares')
+    .doc(code)
+    .get()
+    .then(doc => {
+      if (!doc.exists) {
+        return res.sendStatus(404);
+      } else {
+        return res.send(doc.data());
+      }
+    })
+    .catch(err => res.status(500).send(err));
 }

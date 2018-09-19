db097520f24e66759ef0179c97011d834de7b716
Aaron Haurwitz <aaron.haurwitz@gmail.com>
2018-09-19 08:02:40 -0400

    set up working hosted function

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,26 @@
+const crypto = require('crypto');
 const functions = require('firebase-functions');
+const express = require('express');
+const shortId = require('shortid');
 
-exports.echo = functions.https.onRequest((req, res) => {
-  res.send(req.query.text);
-});
+const app = express();
+
+app.post('/share', makeShareCode);
+app.get('/share/:code', getShare);
+
+const runtimeOpts = {
+  timeoutSeconds: 20,
+  memory: '128MB',
+};
+exports.share = functions.https.onRequest(app);
+
+function makeShareCode(req, res) {
+  const code = shortId.generate();
+  const { cardNames } = req.body;
+
+  res.send({ code, cardNames });
+}
+
+function getShare(req, res) {
+  res.send(req.params.code);
+}

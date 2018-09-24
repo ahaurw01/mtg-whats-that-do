4d529dc548941434bd5150169267978501b7092f
Aaron Haurwitz <aaron.haurwitz@gmail.com>
2018-09-24 08:53:41 -0400

    configure css modules

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
+const withCSS = require('@zeit/next-css');
+
 module.exports = {
+  ...withCSS({
+    cssModules: true,
+    cssLoaderOptions: {
+      importLoaders: 1,
+      localIdentName: '[local]___[hash:base64:5]',
+    },
+  }),
   exportPathMap: function() {
     return {
       '/': { page: '/' },

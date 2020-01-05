5d9a68882408a91c05aab7fc42af840ee90d27cc
Aaron Haurwitz <aaron.haurwitz@gmail.com>
2020-01-05 09:58:57 -0500

    configure initial service worker settings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withCSS = require('@zeit/next-css');
+const withOffline = require('next-offline');
 
-module.exports = {
+module.exports = withOffline({
   ...withCSS({
     cssModules: true,
     cssLoaderOptions: {
@@ -16,4 +17,26 @@ module.exports = {
   devIndicators: {
     autoPrerender: false,
   },
-};
+  generateInDevMode: true,
+  workboxOpts: {
+    runtimeCaching: [
+      {
+        // Match any request that ends with .png, .jpg, .jpeg or .svg.
+        urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+        // Apply a cache-first strategy.
+        handler: 'CacheFirst',
+
+        options: {
+          // Use a custom cache name.
+          cacheName: 'images',
+
+          // Only cache 100 images.
+          expiration: {
+            maxEntries: 100,
+          },
+        },
+      },
+    ],
+  },
+});
